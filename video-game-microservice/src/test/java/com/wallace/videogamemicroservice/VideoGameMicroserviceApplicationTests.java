f90800fd5fbf75de23c61428ec67cacdf7563bd7
cubajosh <70965052+cubajosh@users.noreply.github.com>
2022-09-03 16:15:17 -0400

    Baseline
    This is a empty project with all the dependencies, there is no code here

---
+++ video-game-microservice/src/test/java/com/wallace/videogamemicroservice/VideoGameMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wallace.videogamemicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoGameMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
